8db9c491c3f37f5eb2b0770fa3c3c198718f83a9
Anas <chdr.anas@gmail.com>
2024-05-13 19:50:33 +0530

    first commit

---
+++ src/main/java/com/taskmanagement/controller/TaskController.java
@@ -0,0 +1,49 @@
+package com.taskmanagement.controller;
+
+import com.taskmanagement.entity.Task;
+import com.taskmanagement.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        Task task = taskRepository.findById(id).
+                orElseThrow(() -> new RuntimeException("Task not found fot id " + id));
+        return task;
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found with id " + id));
+        task.setName(taskDetails.getName());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.isCompleted());
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+
+}
